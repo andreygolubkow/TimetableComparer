5b196e37cce295bdfe2ecc1ec88c1134ed14fde1
Andrey Golubkow <andreygolubkow@hotmail.com>
2019-04-12 18:22:15 +0700

    Новая версия приложения. Добавил метрику.

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,15 @@
       </v-container>
     </v-content>
     <v-footer app>
-			<vue-ya-metrica :counter="53251846"
-											enable-hash
-											enable-webvisor></vue-ya-metrica>
+			<v-layout row wrap align-center justify-center>
+			<v-flex xs6>
+			<!-- Yandex.Metrika informer -->
+			<a href="https://metrika.yandex.ru/stat/?id=53251846&amp;from=informer"
+				 target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/53251846/1_0_FFFFFFFF_EFEFEFFF_0_pageviews"
+																						 style="width:80px; height:15px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры)" /></a>
+			<!-- /Yandex.Metrika informer -->
+			</v-flex>
+			</v-layout>
 		</v-footer>
   </v-app>
 </template>
@@ -20,15 +26,13 @@
   import Hello from './components/MainView';
   import AppToolbar from './components/AppToolbar';
 import AppGroupselector from './components/GroupSelector';
-import VueYaMetrica from 'vue-ya-metrica';
 
   export default {
   	name: 'app',
   	components: {
   		AppToolbar,
   		Hello,
-		AppGroupselector,
-		VueYaMetrica
+		AppGroupselector
   	},
 	data () {
 		return {
